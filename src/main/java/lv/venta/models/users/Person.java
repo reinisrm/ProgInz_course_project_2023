4dcbacdde9a5c50171c452bf883328d4e1347164
reinisrm <rreeiinniiss@gmail.com>
2023-06-08 20:41:48 +0300

    Edit: typo

--- src/main/java/lv/venta/models/users/Person.java
+++ src/main/java/lv/venta/models/users/Person.java
@@ -3,12 +3,16 @@ package lv.venta.models.users;
 import java.util.Collection;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -17,11 +21,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
-@MappedSuperclass
+@Table(name = "person_table")
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Person {
 
 	@Setter(value = AccessLevel.NONE)
