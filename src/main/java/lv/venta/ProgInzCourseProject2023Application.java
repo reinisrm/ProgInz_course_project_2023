5a5e7c22808f43f8168a50707d98ca0436f74632
reinisrm <rreeiinniiss@gmail.com>
2023-06-08 19:59:22 +0300

    New: project forcourse project

---
+++ src/main/java/lv/venta/ProgInzCourseProject2023Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgInzCourseProject2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgInzCourseProject2023Application.class, args);
+	}
+
+}
