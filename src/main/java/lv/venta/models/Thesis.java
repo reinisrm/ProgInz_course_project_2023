d4c8988b2c35db9adad6bc764a3e3b07b69f50b8
reinisrm <rreeiinniiss@gmail.com>
2023-06-09 00:19:48 +0300

    Thesis linkage to supervisor

--- src/main/java/lv/venta/models/Thesis.java
+++ src/main/java/lv/venta/models/Thesis.java
@@ -17,6 +17,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lv.venta.models.users.AcademicPersonel;
 import lv.venta.models.users.Student;
 
 //parlikt uz citu tabulu, kurai ir saite ar pasniedzeju, kas to temu piedava
@@ -73,6 +74,11 @@ public class Thesis {
 	@ManyToOne
 	@JoinColumn(name = "Ids")
 	private Student student;
+	
+	@ManyToOne
+	@JoinColumn(name = "Ida")
+	private AcademicPersonel supervisor;
+	//TODO ja nepieciesams, izveidot saiti ar konsultantu/vertetaju utt.
 	
 	
 	
--- src/main/java/lv/venta/models/users/AcademicPersonel.java
+++ src/main/java/lv/venta/models/users/AcademicPersonel.java
@@ -5,11 +5,13 @@ import java.util.Collection;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lv.venta.models.Course;
+import lv.venta.models.Thesis;
 
 @Table(name = "academic_table")
 @Entity
@@ -22,4 +24,9 @@ public class AcademicPersonel extends Person {
 	@Column(name = "Degree")
 	private Degree degree;
 
+	@OneToMany(mappedBy = "supervisor")
+	private Collection<Thesis> thesis;
+	
+	
+	
 }
