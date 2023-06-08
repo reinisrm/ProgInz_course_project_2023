6801befabc9cb990eaf0215dfa7d02a9ab2df3ab
reinisrm <rreeiinniiss@gmail.com>
2023-06-08 21:09:09 +0300

    Add: linkage between Course and Student

--- src/main/java/lv/venta/models/users/Course.java
+++ src/main/java/lv/venta/models/users/Course.java
@@ -2,11 +2,15 @@ package lv.venta.models.users;
 
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -44,7 +48,8 @@ public class Course {
 	@Column(name = "CreditPoints")
 	private int creditPoints;
 	
-
+	@ManyToMany(mappedBy = "debtCourses")
+	private Collection<Student> debtStudents = new ArrayList<Student>();
 	
 	
 
--- src/main/java/lv/venta/models/users/Student.java
+++ src/main/java/lv/venta/models/users/Student.java
@@ -1,7 +1,17 @@
 package lv.venta.models.users;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,7 +23,34 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Student extends Person{
 	
+	//TODO izveidot data jpa notacijas
+	//TODO izveidot validaciju anotacijas
+	//TODO izveidot sasaisti ar Course klasi.
+	@Column(name = "matriculaNo")
+	@NotNull
+	@Size(min = 8, max = 20)
+	@Pattern(regexp = "[0-9]{8,20}")
 	private String matriculaNo;
+	
+	@Column(name = "FinancialDebt")
+	private float financialDebt;
+
+	@ManyToMany
+	@JoinTable(name = "student_debt_courses_table",
+	joinColumns = @JoinColumn(name="Idc"),
+	inverseJoinColumns = @JoinColumn(name="idp"))
+	private Collection<Course> debtCourse = new ArrayList<Course>();
+	
+	
+	
+	public Student(@NotNull @Size(min = 3, max = 15) String name, @NotNull @Size(min = 3, max = 15) String surname,
+			@NotNull @Size(min = 12, max = 12) @Pattern(regexp = "[0-9]{6}-[0-9]{5}", message = "Neatbilstošs personas kods") String personcode,
+			User user, @NotNull @Size(min = 8, max = 20) @Pattern(regexp = "[0-9]{8,20}") String matriculaNo,
+			float financialDebt) {
+		super(name, surname, personcode, user);
+		this.matriculaNo = matriculaNo;
+		this.financialDebt = financialDebt;
+	}
 	
 	
 	
